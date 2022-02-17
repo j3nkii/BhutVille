6ca41e0805ded8e9d47a05149b248c563190005b
Jacob Larson <jacob.larson55112@gmail.com>
2022-02-17 12:19:07 -0600

    updated db

--- database.sql
+++ database.sql
@@ -1,18 +1,12 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
+	"username" VARCHAR (244) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
 
---unsure if JSON is a proper data type
 CREATE TABLE "characters" (
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR (80) UNIQUE NOT NULL,
-	"user_id" INTEGER REFERENCES "user"(id),
-	"game_state" JSON DEFAULT null
+	"name" VARCHAR (80) NOT NULL,
+	"user_id" INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
+	"game_state" JSONB DEFAULT null
 );
